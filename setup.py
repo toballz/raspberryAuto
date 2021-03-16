70a6c8ae54f1858c79a8e040cdfdd45d3f588a68
toballz <42551340+toballz@users.noreply.github.com>
2021-03-15 21:17:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,18 +9,6 @@ print("\n[~] do updates")
 os.system("sudo apt-get install bluez")
 print("\n[~] copy exe")
 os.system("sudo cp "+dirr+"/p /usr/local/bin/p")
-print("\n[~] exe name \"p [command]\" exe\n")
-if(os.path.exists("/etc/rc.local") and os.path.isfile("/etc/rc.local")):
- print("[~] edit file")
- os.system("sudo cat /etc/rc.local > "+dirr+"/rc.local")
- with open(dirr+"/rc.local", "r") as xfil:
-  ctr = xfil.read().replace("exit 0", dirr+"\nexit 0")
-  os.system("sudo rm /etc/rc.local")
-  os.system("sudo echo "+ctr+" > "+dirr+"/rc.local")
-  os.system("sudo cp "+dirr+"/rc.local /etc/rc.local")
-else:
- print("[~] create file")
 
- os.system("sudo chmod 777 "+dirr+" | sudo chmod 777 "+dirr+"*")
- os.system("sudo echo '#!/bin/sh -e\n\nsudo bash "+dirr+"/startup.sh\n\nexit 0' >> "+dirr+"/rc.local")
- os.system("sudo cp "+dirr+"/rc.local /etc/")
+print("\n[~] exe name \"p [command]\" exe\n")
+print("crontab -e\n@reboot [filePath]")
